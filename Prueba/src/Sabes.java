755fffeebd6aac560f618f37ca714892f06970b6
carrry2000 <dracontcito@gmail.com>
2019-04-08 17:16:56 +0200

    cuarta subida

--- Prueba/src/Sabes.java
+++ Prueba/src/Sabes.java
@@ -3,9 +3,11 @@ public class Sabes {
 	public static void main(String[] args) {
 		int a = 5;
 		int b=9;
+		int resta= (b-a);
 		int suma = (a*b);
 		System.out.println(suma);
 		System.out.println("Matthew es muy pesado");
 		System.out.println("Fin del programa");//github.com/carrry2000/Programacion.git
+		System.out.println(resta);
 	}
 }
